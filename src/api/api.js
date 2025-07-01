9d5e3d1e7a29c7fab2eec820301cc2fa15d60e96
= <=>
2025-07-01 08:51:42 +0100

    fix: corrigit bug de autorizacao

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "http://localhost:5000/academia.egaf.ao"
+const url = "https://academia-egaf-api.onrender.com/academia.egaf.ao"
 const api = axios.create(({
     baseURL: url,
     withCredentials: true,
