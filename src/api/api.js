577f5216ab8302997418df09af26882eda2f8732
= <=>
2025-07-04 15:28:19 +0100

    updades

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const url = "https://academia-egaf-api.onrender.com/academia.egaf.ao"
-const videoApiUrl = "http://localhost:5350/academia.egaf.ao" 
+const videoApiUrl = "http://192.168.88.20:5350/academia.egaf.ao" 
 
 const api = axios.create({
     baseURL: url,
